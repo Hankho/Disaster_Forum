3e728f3a7cb70ebbfa72f9928717ba8ded03e201
Hank <hankhe@gmail.com>
2016-09-08 15:34:54 +0800

    use_form_partial

--- db/migrate/20160902101325_create_messages.rb
+++ db/migrate/20160902101325_create_messages.rb
@@ -3,7 +3,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
     create_table :messages do |t|
     t.string :message
     t.integer :disaster_id
-      t.timestamps
+    t.timestamps
     end
   end
 end
