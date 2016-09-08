297ca2a7c7a64ceacb8a1b69c07595061f58fb7b
Hank <hankhe@gmail.com>
2016-09-08 16:33:09 +0800

    add_About

--- app/controllers/disasters_controller.rb
+++ app/controllers/disasters_controller.rb
@@ -7,6 +7,12 @@ class DisastersController < ApplicationController
 	def new
 		@disaster = Disaster.new
 	end
+	# GET /disasters/about
+	def about
+		@users = User.all
+		@messages = Message.all
+		@disasters = Disaster.all
+	end
 	def show
 		@messages = @disaster.messages
 	    @disaster_message = Message.new  #new出來的物件用來給form_for塞進來的資料
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 	resources :disasters do
 			resources :messages, :controller=>"disaster_messages"
+
+		collection do 
+			get :about	
+		end
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
